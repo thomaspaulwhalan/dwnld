6d23e87ffd55cf579fa55f4b12ae834ba1ebd087
thomaspaulwhalan <78250325+thomaspaulwhalan@users.noreply.github.com>
2021-08-09 12:26:25 +1000

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,17 @@
+#include<dwnld.h>
 
+int main(int argc, char **argv)
+{
+	if (argc <= 1) {
+		fprintf(stderr, "dwnld: not enough arguments\n");
+		return 1;
+	}
+	else if (argc >= 3) {
+		fprintf(stderr, "dwnld: too many arguments\n");
+		return 1;
+	}
+
+	dwnld(argv[1], "80");
+
+	return 0;
+}
